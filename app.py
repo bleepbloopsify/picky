2fa715bc9e3150b9b3422c711710f189527e6b15
bleepbloopsify <bleepbloopsify@gmail.com>
2015-10-24 14:33:52 -0400

    bleh

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
 import util
 
 app = Flask(__name__)
@@ -12,10 +12,18 @@ def index():
 def login():
     return render_template('login.html')
 
-@app.route('/register')
+@app.route('/register', methods=["GET","POST"])
 def register():
+    if request.method == "POST":
+        form = request.form
+        user = form.get('user')
+        pwd = form.get('pwd')
+        pwdtwo = form.get('pwd2')
+        if pwd != pwdtwo:
+            return render_template('register.html')
+        util.new_user(user,pwd)
     return render_template('register.html')
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     app.debug = True
     app.run()
--- util.py
+++ util.py
@@ -1,22 +1,28 @@
 import pymongo
-from pymongo import MongoClient()
-import md5
+from pymongo import MongoClient
+import hashlib
 
 
 client = MongoClient()
 db = client['database']
 
+userlist = db.users
+
 
 def new_user(user,pwd):
-    
+    m = hashlib.sha224(pwd)
+    password = m.hexdigest()
+    newuser = {"username":user, "pass":password }
+    post_id = userlist.insert_one(newuser).inserted_id
 
+def authenticate(user, pwd):
+    m = sha224.new()
+    m.update(pwd)
+    password = m.hexdigest()
+    if (userlist.find_one({"username":user}) == None):
+        return "error"
 
-    return True
-
-def authenticate():
-    return True
-
-def record_restaurant():
+#def record_restaurant():
 
 def pull():
     return ""
