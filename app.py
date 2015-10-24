dd0e329ec92a00e731764f94b383d82cf713c4c0
bleepbloopsify <bleepbloopsify@gmail.com>
2015-10-24 13:52:10 -0400

    init2

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import util
 
 app = Flask(__name__)
 app.secret_key = "nothing"
@@ -6,3 +7,15 @@ app.secret_key = "nothing"
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+if __name__ = '__main__':
+    app.debug = True
+    app.run()
---
+++ util.py
@@ -0,0 +1,25 @@
+import pymongo
+from pymongo import MongoClient()
+import md5
+
+
+client = MongoClient()
+db = client['database']
+
+
+def new_user(user,pwd):
+    
+
+
+    return True
+
+def authenticate():
+    return True
+
+def record_restaurant():
+
+def pull():
+    return ""
+
+def filter():
+    return ["mcd"]
